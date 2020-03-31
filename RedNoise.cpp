4244b0fb0748d703ba0fd9fbaa3b3ed555ac78bf
Ben-Fozard <bf17813@bristol.ac.uk>
2020-03-31 16:07:20 +0100

    Lab1 done

--- RedNoise.cpp
+++ RedNoise.cpp
@@ -15,6 +15,8 @@ using namespace glm;
 void draw();
 void update();
 void handleEvent(SDL_Event event);
+std::vector<float> interpolate(float from, float to, int numberOfValues);
+std::vector<vec3> newInterpolate(vec3 from, vec3 to, int numberOfValues);
 
 DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 
@@ -35,12 +37,38 @@ int main(int argc, char* argv[])
 void draw()
 {
   window.clearPixels();
+  //TOP LEFT, RED, 255,0,0
+  //TOP RIGHT, BLUE, 0,0,255
+  //BOTTOM LEFT, YELLOW, 255, 255, 0
+  //BOTTOM RIGHT, GREEN, 0,255,0
+  //
+  //
+  //
+
+  std::vector<vec3> right = newInterpolate(vec3(0,255,0), vec3(0,0,255), window.height);
+  std::vector<vec3> left = newInterpolate(vec3(255,255,0), vec3(255,0,0), window.height);
+
   for(int y=0; y<window.height ;y++) {
+
+    // std::vector<float> redi = interpolate(0, 255, window.width);
+    // std::vector<float> greeni = interpolate(0, 255, window.width);
+    // std::vector<float> bluei = interpolate(0, 255, window.width);
+    vec3 lefti = left.back();
+    left.pop_back();
+    vec3 righti = right.back();
+    right.pop_back();
+
+    std::vector<vec3> pixel = newInterpolate(righti, lefti, window.width);
+
     for(int x=0; x<window.width ;x++) {
-      float red = rand() % 255;
-      float green = 0.0;
-      float blue = 0.0;
-      uint32_t colour = (255<<24) + (int(red)<<16) + (int(green)<<8) + int(blue);
+      // float red = rand() % 255;
+      // float green = 0.0;
+      // float blue = 0.0;
+      uint32_t colour = (255<<24) + (int(pixel.back().x)<<16) + (int(pixel.back().y)<<8) + int(pixel.back().z);
+      pixel.pop_back();
+      // redi.pop_back();
+      // greeni.pop_back();
+      // bluei.pop_back();
       window.setPixelColour(x, y, colour);
     }
   }
@@ -60,4 +88,31 @@ void handleEvent(SDL_Event event)
     else if(event.key.keysym.sym == SDLK_DOWN) cout << "DOWN" << endl;
   }
   else if(event.type == SDL_MOUSEBUTTONDOWN) cout << "MOUSE CLICKED" << endl;
+}
+
+std::vector<float> interpolate(float from, float to, int numberOfValues) {
+  std::vector<float> results = {};
+
+  float step = (to - from)/(numberOfValues - 1);
+
+  for (int i = 0; i < numberOfValues; i++) {
+    results.push_back(from + (i*step));
+  }
+
+  return results;
+}
+
+std::vector<vec3> newInterpolate(vec3 from, vec3 to, int numberOfValues) {
+  std::vector<vec3> results= {};
+
+  float stepX = (to.x - from.x)/(numberOfValues - 1);
+  float stepY = (to.y - from.y)/(numberOfValues - 1);
+  float stepZ = (to.z - from.z)/(numberOfValues - 1);
+
+  for (int i = 0; i < numberOfValues; i++){
+    vec3 v = vec3(from.x + (i*stepX), from.y + (i*stepY), from.z + (i*stepZ));
+    results.push_back(v);
+  }
+
+  return results;
 }
